9fbb98bd6640bd11d0b2658d22cc9c3f5f0750ff
TMDStudios <tmdstudioscontact@gmail.com>
2024-01-09 22:10:48 -0500

    Deployed, updated README, fixed default apps

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Indie Game Explorer
 
+![Railway Badge](https://img.shields.io/badge/deployment-railway-purple)
+
 A simple one-page website to showcase indie games.
+
+>You can see the website here: https://ige.up.railway.app/
+
+## Key Features
+
+- The website is connected to a Rest API that provides all game data
+- Anyone can add a game by filling out the provided form
+- Once submitted, a game needs to be approved before it is displayed on the website
+- Currently only YouTube shorts are the accepted video format
+
+## Future Plans
+
+- An image will be displayed if no video link is provided
+- Support for more game types
+- Better game descriptions
 
 ## You may also like...
 
--- app.js
+++ app.js
@@ -1,63 +1,64 @@
-let apps = []
+let apps = [];
 
 function getApps(){
     const request = new XMLHttpRequest();
     request.open("GET", "https://devroboto.pythonanywhere.com/games/");
     request.onload = function(){
-        console.log("Got the apps")
+        console.log("Got the apps");
         const data = JSON.parse(request.response);
         for(let i = 0; i<data.length; i++){
             apps.push(data[i]);
         }
-        if(apps.length==0){
-            return [
-                {
-                    "name": "Block Wars",
-                    "description": "Battle enemy ships in a shrinking environment.",
-                    "website": "https://tmdstudios.github.io/",
-                    "app_store": "",
-                    "google_play": "https://play.google.com/store/apps/details?id=com.tmdstudios.blockwars",
-                    "video": "9MBChUsqMy8",
-                    "image": "https://play-lh.googleusercontent.com/1rON5qf8rvD3w948-2sKvFVzu0ZSEXTclvhF1PFw8mYxb74L60VeZDLQ-7OnVlXRG1I=w2560-h1440-rw",
-                    "genre": "Arcade",
-                    "approved": 1
-                },
-                {
-                    "name": "Buddy Breakout",
-                    "description": "A new twist on a classic.",
-                    "website": "https://tmdstudios.github.io/",
-                    "app_store": "",
-                    "google_play": "https://play.google.com/store/apps/details?id=com.tmdstudios.buddybreakout",
-                    "video": "V1wk2NYxhmk",
-                    "image": "https://play-lh.googleusercontent.com/M8Kb7P8kKtzAbiVWDPJ8zQr4Mbeekv2kgWl6tGZyWMuv-XdjEZeJz9t9hpviBdEF1w=w2560-h1440-rw",
-                    "genre": "Arcade",
-                    "approved": 1
-                },
-                {
-                    "name": "Puzzled Knight",
-                    "description": "A match-3 fighting game.",
-                    "website": "https://tmdstudios.github.io/",
-                    "app_store": "",
-                    "google_play": "https://play.google.com/store/apps/details?id=com.tmdstudios.puzzledknight",
-                    "video": "aYcv7Hl_kB8",
-                    "image": "https://play-lh.googleusercontent.com/NHQEB2nxZP8Iy5SJwAX5T33dUABP-WKVN5Xnww5WDvfOqGuKMc2UZj6TKNTcDHrCkC0=w2560-h1440-rw",
-                    "genre": "Puzzle",
-                    "approved": 1
-                },
-                {
-                    "name": "Road to Rage",
-                    "description": "Shoot your way through a horde of inconsiderate motorists.",
-                    "website": "https://tmdstudios.github.io/",
-                    "app_store": "",
-                    "google_play": "https://play.google.com/store/apps/details?id=com.wordpress.tmdstudios",
-                    "video": "r-tbHSkd03Y",
-                    "image": "https://play-lh.googleusercontent.com/I1m1d0nO00mpXQqbt117pz2ltcNf2Bq9CpprbR0-BI-9m8OZVAMf0bGSIgzyguwHdgOP=w2560-h1440-rw",
-                    "genre": "Arcade",
-                    "approved": 1
-                }
-            ]
-        }
-        return JSON.parse(request.response)
+        return JSON.parse(request.response);
+    }
+    request.onerror = _ => {
+        console.log("Unable to load apps, using default.");
+        apps = [
+            {
+                "name": "Block Wars",
+                "description": "Battle enemy ships in a shrinking environment.",
+                "website": "https://tmdstudios.github.io/",
+                "app_store": "1",
+                "google_play": "https://play.google.com/store/apps/details?id=com.tmdstudios.blockwars",
+                "video": "9MBChUsqMy8",
+                "image": "https://play-lh.googleusercontent.com/1rON5qf8rvD3w948-2sKvFVzu0ZSEXTclvhF1PFw8mYxb74L60VeZDLQ-7OnVlXRG1I=w2560-h1440-rw",
+                "genre": "Arcade",
+                "approved": 1
+            },
+            {
+                "name": "Buddy Breakout",
+                "description": "A new twist on a classic.",
+                "website": "https://tmdstudios.github.io/",
+                "app_store": "2",
+                "google_play": "https://play.google.com/store/apps/details?id=com.tmdstudios.buddybreakout",
+                "video": "V1wk2NYxhmk",
+                "image": "https://play-lh.googleusercontent.com/M8Kb7P8kKtzAbiVWDPJ8zQr4Mbeekv2kgWl6tGZyWMuv-XdjEZeJz9t9hpviBdEF1w=w2560-h1440-rw",
+                "genre": "Arcade",
+                "approved": 1
+            },
+            {
+                "name": "Puzzled Knight",
+                "description": "A match-3 fighting game.",
+                "website": "https://tmdstudios.github.io/",
+                "app_store": "3",
+                "google_play": "https://play.google.com/store/apps/details?id=com.tmdstudios.puzzledknight",
+                "video": "aYcv7Hl_kB8",
+                "image": "https://play-lh.googleusercontent.com/NHQEB2nxZP8Iy5SJwAX5T33dUABP-WKVN5Xnww5WDvfOqGuKMc2UZj6TKNTcDHrCkC0=w2560-h1440-rw",
+                "genre": "Puzzle",
+                "approved": 1
+            },
+            {
+                "name": "Road to Rage",
+                "description": "Shoot your way through a horde of inconsiderate motorists.",
+                "website": "https://tmdstudios.github.io/",
+                "app_store": "4",
+                "google_play": "https://play.google.com/store/apps/details?id=com.wordpress.tmdstudios",
+                "video": "r-tbHSkd03Y",
+                "image": "https://play-lh.googleusercontent.com/I1m1d0nO00mpXQqbt117pz2ltcNf2Bq9CpprbR0-BI-9m8OZVAMf0bGSIgzyguwHdgOP=w2560-h1440-rw",
+                "genre": "Arcade",
+                "approved": 1
+            }
+        ]
     }
     request.send();
 }
@@ -118,9 +119,9 @@ function viewImage(){
     window.open("/"+apps[currentApp].fullImg);
 }
 
-const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
-window.addEventListener('resize', appHeight)
-appHeight()
+const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
+window.addEventListener('resize', appHeight);
+appHeight();
 
 // MODAL
 function addGame(){
@@ -129,13 +130,14 @@ function addGame(){
 
 let gameForm = document.getElementById("addGameForm");
 gameForm.onsubmit = function(e){
-    e.preventDefault()
+    e.preventDefault();
     console.log("Submit Game");
 
     let xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://devroboto.pythonanywhere.com/games/add/");
     xhttp.onload = function(){
-        alert("Game submitted")
+        // Replace with modal
+        alert("Game submitted\n\nNOTE: The game must be approved before it is officially added to the database");
     }
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     if(document.getElementById('gameName').value.length>0
@@ -151,7 +153,8 @@ gameForm.onsubmit = function(e){
         "&genre="+document.getElementById('genre').value
         );
     }else{
-        alert("Game Name, Description, and Image must be provided")
+        // Replace with modal
+        alert("Game Name, Description, and Image must be provided");
     }
     document.getElementById("modal").close();
 }
